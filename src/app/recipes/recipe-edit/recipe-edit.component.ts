2274ceacaebae624fe460fcf5ed0ff19806de05b
Jan Bańczerowski <jasiuu2605@gmail.com>
2023-09-16 20:31:01 +0200

    creating reactive form for editing recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,24 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Params } from "@angular/router";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  selector: "app-recipe-edit",
+  templateUrl: "./recipe-edit.component.html",
+  styleUrls: ["./recipe-edit.component.css"],
 })
 export class RecipeEditComponent implements OnInit {
-
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
-      this.route.params.subscribe((params: Params) => {
-        this.id = params['id'];
-        this.editMode = params['id'] != null;
-        console.log(this.editMode);
-      })
+    this.route.params.subscribe((params: Params) => {
+      this.id = params["id"];
+      this.editMode = params["id"] != null;
+      this.initForm()
+    });
   }
 
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      recipeDescription: new FormControl(recipeDescription)
+    });
+  }
 }
