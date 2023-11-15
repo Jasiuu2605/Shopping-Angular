437cc06762c4f16186a6a678f48f73b75748693b
Jan Bańczerowski <jasiuu2605@gmail.com>
2023-11-15 20:38:21 +0100

    added auth service and basic logic

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  isLoginMode = true;
 
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode
+  }
+
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset()
+  }
 }
