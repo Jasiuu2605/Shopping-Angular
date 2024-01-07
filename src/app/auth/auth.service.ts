9987c4245083022d1619f37aa2d5f0ac9d892b61
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-01-07 17:36:56 +0100

    added signup functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
+import { catchError } from "rxjs";
+import { throwError } from "rxjs";
 interface AuthResponseData {
+  kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
@@ -10,22 +12,37 @@ interface AuthResponseData {
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-
-    constructor(private http: HttpClient) {}
-
-
-    signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>(
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAVQa_RM2Ay9r0E96g4E4iCNftENaKqKWs",
-          {
-            email: email,
-            password: password,
-            returnSecureToken: true
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAVQa_RM2Ay9r0E96g4E4iCNftENaKqKWs",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = "An unknown error occured";
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
           }
-        );
-    }   
+          switch (errorRes.error.error.message) {
+            case "EMAIL_EXISTS":
+              errorMessage = "This email already exists";
+              break;
+
+            default:
+              break;
+          }
+          return throwError(errorMessage)
+        })
+      );
+  }
 }
